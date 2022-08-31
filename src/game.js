12e8f26c232a0eee4ea76b2250d940be9ac0ad33
Dosaki <lord.g.bug@gmail.com>
2022-08-31 21:39:35 +0100

    Fixed issue with moving something that's gone

--- src/game.js
+++ src/game.js
@@ -79,7 +79,6 @@ ct.width = windowSize;
 ct.height = windowSize;
 const tutorialCtx = ct.getContext('2d');
 
-const menu = document.querySelector('[m]');
 menu.style = `width:${windowSize}px;height:${windowSize}px`;
 
 const map = new Map(50, 20, W);
@@ -98,28 +97,28 @@ let isTutorial = 0;
 let tutorialSteps = [
     { m: "Welcome to Hell Inc. upper management!", pgc: true },
     { m: "You've been hand-plucked from the demonic masses to manage this hell pit", pgc: true },
-    { m: "We'll be monitoring your misery extraction progress", clear: [ui.r(125), ui.r(1000), ui.r(300), ui.r(70)], al: [ui.r(208), ui.r(1000)], pgc: true },
-    { m: "This is the soul waiting area", clear: [ui.r(321), ui.r(349), ui.r(60), ui.r(50)], al: [ui.r(351), ui.r(399)], pgc: true },
-    { m: "Click this soul", clear: [ui.r(333), ui.r(376), ui.r(20), ui.r(25)], al: [ui.r(344), ui.r(396)], cc: true, rwp: true },
-    { m: "Sinful souls give you more misery but they can lie about their sins", clear: [ui.r(3), ui.r(3), ui.r(-176), ui.r(-2)], at: [0, 0], cpu: true, pgc: true },
-    { m: "This gives you more info about this soul", clear: [ui.r(45), ui.r(5), ui.r(-48), ui.r(-30)], at: [0, 0], cpu: true, pgc: true },
-    { m: "Weak souls may perish during torture, so you want sturdy ones", clear: [ui.r(45), ui.r(5), ui.r(-48), ui.r(-30)], at: [0, 0], cpu: true, pgc: true },
+    { m: "We'll be monitoring your misery extraction progress", clear: [125, 1000, 300, 70], al: [208, 1000], pgc: true },
+    { m: "This is the soul waiting area", clear: [321, 349, 60, 50], al: [351, 399], pgc: true },
+    { m: "Click this soul", clear: [333, 376, 20, 25], al: [344, 396], cc: true, rwp: true },
+    { m: "Sinful souls give you more misery but they can lie about their sins", clear: [3, 3, -176, -2], at: [0, 0], cpu: true, pgc: true },
+    { m: "This gives you more info about this soul", clear: [45, 5, -48, -30], at: [0, 0], cpu: true, pgc: true },
+    { m: "Weak souls may perish during torture, so you want sturdy ones", clear: [45, 5, -48, -30], at: [0, 0], cpu: true, pgc: true },
     { m: "Souls perish if waiting for too long", pgc: true },
     { m: "Don't let souls perish without extracting misery!", pgc: true },
-    { m: "Click ✔ to accept this soul", clear: [ui.r(165), ui.r(65), ui.r(-170), ui.r(-70)], at: [0, 0], cpu: true, cc: true },
-    { m: "The soul gave you some coins. You now have 22 coins!", clear: [ui.r(3), ui.r(1000), ui.r(110), ui.r(70)], al: [ui.r(58), ui.r(1000)], pgc: true },
-    { m: "It's got nowhere to go, so it's waiting", clear: [ui.r(321), ui.r(349), ui.r(60), ui.r(50)], al: [ui.r(351), ui.r(399)], pgc: true },
-    { m: "Click the Dispair Room", clear: [ui.r(236), ui.r(856), ui.r(115), ui.r(115)], al: [ui.r(293), ui.r(856)], cc: true },
-    { m: "Then click your pit", clear: [ui.r(422), ui.r(311), ui.r(70), ui.r(70)], at: [ui.r(457), ui.r(381)], cc: true },
+    { m: "Click ✔ to accept this soul", clear: [165, 65, -170, -70], at: [0, 0], cpu: true, cc: true },
+    { m: "The soul gave you some coins. You now have 22 coins!", clear: [3, 1000, 110, 70], al: [58, 1000], pgc: true },
+    { m: "It's got nowhere to go, so it's waiting", clear: [321, 349, 60, 50], al: [351, 399], pgc: true },
+    { m: "Click the Dispair Room", clear: [236, 856, 115, 115], al: [293, 856], cc: true },
+    { m: "Then click your pit", clear: [422, 311, 70, 70], at: [457, 381], cc: true },
     { m: "The soul will slowly travel to the machine", pgc: true },
-    { m: "Paths let them travel faster", clear: [ui.r(10), ui.r(856), ui.r(115), ui.r(115)], al: [ui.r(67), ui.r(856)], pgc: true },
-    { m: "Click your Dispair Room", clear: [ui.r(422), ui.r(311), ui.r(70), ui.r(70)], at: [ui.r(457), ui.r(381)], cc: true, rwp: true },
-    { m: "This shows if the machine is working and who operates it", clear: [ui.r(0), ui.r(0), ui.r(0), ui.r(0)], at: [ui.r(0), ui.r(0)], cpu: true, pgc: true },
+    { m: "Paths let them travel faster", clear: [10, 856, 115, 115], al: [67, 856], pgc: true },
+    { m: "Click your Dispair Room", clear: [422, 311, 70, 70], at: [457, 381], cc: true, rwp: true },
+    { m: "This shows if the machine is working and who operates it", clear: [0, 0, 0, 0], at: [0, 0], cpu: true, pgc: true },
     { m: "When the soul's misery is full you can extract it", pgc: true },
-    { m: "Build a Misery Extractor", clear: [ui.r(120), ui.r(856), ui.r(115), ui.r(115)], al: [ui.r(177), ui.r(856)], cc: true },
-    { m: "Then click your pit", clear: [ui.r(522), ui.r(411), ui.r(70), ui.r(70)], at: [ui.r(557), ui.r(481)], cc: true },
-    { m: "Now employ a demon for it", clear: [ui.r(854), ui.r(856), ui.r(236), ui.r(236)], ar: [ui.r(972), ui.r(856)], cc: true },
-    { m: "Then click your Misery Extractor", clear: [ui.r(522), ui.r(411), ui.r(70), ui.r(70)], at: [ui.r(557), ui.r(481)], cc: true },
+    { m: "Build a Misery Extractor", clear: [120, 856, 115, 115], al: [177, 856], cc: true },
+    { m: "Then click your pit", clear: [522, 411, 70, 70], at: [557, 481], cc: true },
+    { m: "Now employ a demon for it", clear: [854, 856, 236, 236], ar: [972, 856], cc: true },
+    { m: "Then click your Misery Extractor", clear: [522, 411, 70, 70], at: [557, 481], cc: true },
     { m: "That concludes your training. Have a miserable time!", pgc: true }
 ];
 const uiCtx = cui.getContext('2d');
@@ -239,7 +238,7 @@ const main = function () {
                 } else {
                     rect = tutorialSteps[0].clear;
                 }
-                tutorialCtx.clearRect(...rect);
+                tutorialCtx.clearRect(...rect.map(value => ui.r(value)));
             }
             tutorialCtx.fillStyle = "#ffffff";
             tutorialCtx.font = `${ui.r(24)}px luminari, fantasy`;
@@ -250,30 +249,30 @@ const main = function () {
             if (tutorialSteps[0].al) {
                 tutorialCtx.beginPath();
                 tutorialCtx.moveTo((windowSize / 2 - measurement.width / 2) - ui.r(10), (windowSize / 2) - ui.r(24) / 2);
-                tutorialCtx.lineTo(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].al[0] : tutorialSteps[0].al[0], (windowSize / 2) - ui.r(24) / 2);
+                tutorialCtx.lineTo(ui.r(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].al[0] : tutorialSteps[0].al[0]), (windowSize / 2) - ui.r(24) / 2);
                 tutorialCtx.lineTo(
-                    tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].al[0] : tutorialSteps[0].al[0],
-                    tutorialSteps[0].cpu === true ? rect[1] + (rect[3] / 2) + tutorialSteps[0].al[1] : tutorialSteps[0].al[1]);
+                    ui.r(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].al[0] : tutorialSteps[0].al[0]),
+                    ui.r(tutorialSteps[0].cpu === true ? rect[1] + (rect[3] / 2) + tutorialSteps[0].al[1] : tutorialSteps[0].al[1]));
                 tutorialCtx.strokeStyle = "#ffffff";
                 tutorialCtx.stroke();
             }
             if (tutorialSteps[0].at) {
                 tutorialCtx.beginPath();
-                tutorialCtx.moveTo(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].at[0] : tutorialSteps[0].at[0], (windowSize / 2) - ui.r(24));
+                tutorialCtx.moveTo(ui.r(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].at[0] : tutorialSteps[0].at[0]), (windowSize / 2) - ui.r(24));
                 tutorialCtx.lineTo(
-                    tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].at[0] : tutorialSteps[0].at[0],
-                    tutorialSteps[0].cpu === true ? rect[1] + rect[3] + tutorialSteps[0].at[1] : tutorialSteps[0].at[1]);
+                    ui.r(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].at[0] : tutorialSteps[0].at[0]),
+                    ui.r(tutorialSteps[0].cpu === true ? rect[1] + rect[3] + tutorialSteps[0].at[1] : tutorialSteps[0].at[1]));
                 tutorialCtx.strokeStyle = "#ffffff";
                 tutorialCtx.stroke();
             }
             if (tutorialSteps[0].ar) {
                 tutorialCtx.beginPath();
                 tutorialCtx.moveTo((windowSize / 2 + measurement.width / 2) + ui.r(10), (windowSize / 2) - ui.r(24) / 2);
-                tutorialCtx.lineTo(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].ar[0] : tutorialSteps[0].ar[0], (windowSize / 2) - ui.r(24) / 2);
+                tutorialCtx.lineTo(ui.r(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].ar[0] : tutorialSteps[0].ar[0]), (windowSize / 2) - ui.r(24) / 2);
                 tutorialCtx.lineTo(tutorialSteps[0].ar[0], (windowSize / 2) - ui.r(24) / 2);
                 tutorialCtx.lineTo(
-                    tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].ar[0] : tutorialSteps[0].ar[0],
-                    tutorialSteps[0].cpu === true ? rect[1] + (rect[3] / 2) + tutorialSteps[0].ar[1] : tutorialSteps[0].ar[1]);
+                    ui.r(tutorialSteps[0].cpu === true ? rect[0] + (rect[2] / 2) + tutorialSteps[0].ar[0] : tutorialSteps[0].ar[0]),
+                    ui.r(tutorialSteps[0].cpu === true ? rect[1] + (rect[3] / 2) + tutorialSteps[0].ar[1] : tutorialSteps[0].ar[1]));
                 tutorialCtx.strokeStyle = "#ffffff";
                 tutorialCtx.stroke();
             }
