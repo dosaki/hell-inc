a73e5eb7f009c74f1b04db7aa647fefa0f00e6d6
Dosaki <lord.g.bug@gmail.com>
2022-09-03 20:44:01 +0100

    Correcting iron maiden texture

--- src/entities/resources.js
+++ src/entities/resources.js
@@ -15,7 +15,7 @@ class Resources {
             new Machine("Path", 1, false, 0, 1, createPerlinImage(4, [0, 1, 2]), "plane", 1, 1),
             new Machine("Misery Extractor", 5, true, 0, 1, createPerlinImage(4, [1, 2]), "cube", 3, 3, 5),
             new Machine("Dispair Room", 10, false, 1, 1, createPerlinImage(8, [0, 2]), "cube", 5, 5, 8),
-            new Machine("Iron Maiden", 25, true, 2, 2, createPerlinImage(2, [0, 1]), "cube", 1, 1, 2),
+            new Machine("Iron Maiden", 25, true, 2, 2, createPerlinImage(4, [0, 1]), "cube", 1, 1, 2),
             new Machine("Cauldron", 50, true, 3, 3, createPerlinImage(4, [1]), "cube", 3, 3, 1),
             new Machine("Blender", 100, true, 4, 4, createPerlinImage(4, [2]), "cube", 1, 1, 5),
             new Machine("Javascript Terminal", 150, false, 5, 5, createPerlinImage(2, []), "cube", 1, 1, 2)
